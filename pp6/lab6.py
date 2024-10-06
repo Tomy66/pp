4768482976738c0ddccc999ab5172950ec546410
matmeka <matmeka6@gmail.com>
2024-10-06 16:22:13 +0200

    Update

--- pp6/lab6.py
+++ pp6/lab6.py
@@ -1,28 +1,25 @@
 # 1 ćwiczenie
-a = str(input("Podaj znak: "))
-b = float(input("Podaj liczbę kolumn: "))
-c = float(input("Podaj liczbę wierszy: "))
+a = input("Podaj znak: ")
+b = int(input("Podaj liczbę kolumn: "))
+c = int(input("Podaj liczbę wierszy: "))
 
-print((a * int(b) + "\n") * int(c))
+print((a * b + "\n") * c)
 
 print()
 
 # 2 ćwiczenie
 
-a = input("Podaj pierwszą liczbę całkowitą: ")
-b = input("Podaj drugą liczbę całkowitą: ")
-
-print("Wynik dodawania tych licz to:", int(a) + int(b))
-print("Wynik odejmowania tych licz to:",int(a) - int(b))
-print("Wynik mnożenia tych licz to:",int(a) * int(b))
-print("Wynik dzielenia tych licz to:",int(a) / int(b))
-print("Reszta z dzielenia:",int(a) % int(b))
+a = int(input("Podaj pierwszą liczbę całkowitą: "))
+b = int(input("Podaj drugą liczbę całkowitą: "))
+print()
+print("Wynik dodawania tych licz to:", a + b)
+print("Wynik odejmowania tych licz to:", a - b)
+print("Wynik mnożenia tych licz to:", a * b)
+print("Wynik dzielenia tych licz to:", a / b)
+print("Reszta z dzielenia:", a % b)
 
 print()
 
 # 3 ćwiczenie
 
 print("+" + 9 * "-+" + "\n" + "|" + 9 * " |" + "\n" + "+" + 9 * "-+")
-
-
-
